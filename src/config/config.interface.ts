18cd371c373a9f1269158afbb46fbcda5affbc05
Zaid Iqbal <zaid.njs@tafsol.com>
2024-09-30 18:49:07 -0700

    ref: udpated gitignore and added config interface

--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -1,1 +1,21 @@
-export interface EnvConfig {}
+export interface EnvConfig {
+  //BASIC CREDENTIALS
+  PORT: number;
+  APP_NAME: string;
+  APP_VERSION: string;
+  WEB_HOSTED_URL: string;
+  API_HOSTED_URL: string;
+
+  // JWT CREDENTIALS
+  JWT_SECRET: string;
+  JWT_EXPIRES_IN: string;
+
+  // DATABASE CREDENTIALS
+  MONGODB_URI: string;
+  MONGODB_PASSWORD: string;
+
+  // SENDGRID CREDENTIALS
+  EMAIL_FROM: string;
+  SENDGRID_USERNAME: string;
+  SENDGRID_PASSWORD: string;
+}
